f5bcac503ac8cb23d27b9ed2fa02fe88c9fa64ff
AnneClr <anneclaire.fougerit@gmail.com>
2024-08-23 10:19:21 +0200

    add use-debounce package to optimize handleSearch

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,22 +1,36 @@
 'use client';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter()
-  const handleSearch = (term: string) => {
-    console.log(`Searching...${term}`);
+  // const handleSearch = (term: string) => {
+  //   console.log(`Searching...${term}`);
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set('query', term)
+  //   } else {
+  //     params.delete('query')
+  //   }
+
+  //   replace(`${pathname}?${params.toString()}`)
+  // };
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('query', term)
+      params.set('query', term);
     } else {
-      params.delete('query')
+      params.delete('query');
     }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
-    replace(`${pathname}?${params.toString()}`)
-  };
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
